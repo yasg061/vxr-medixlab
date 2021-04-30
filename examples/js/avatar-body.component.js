bb0dff5d64d6237ff301eb3883b7f5e4038f54e7
EnriqueVega1995 <vegacampos1995@gmail.com>
2021-04-29 20:14:04 -0600

    Modified avatar body component and modified vxr host template

--- examples/js/avatar-body.component.js
+++ examples/js/avatar-body.component.js
@@ -1,16 +1,19 @@
 /* global AFRAME */
 AFRAME.registerComponent('avatar-body', {
   schema: {
-      body: { type: 'selector'}
-      
+    body: {type: 'selector'},
+    id: {type: 'string' }  
     },
   init: function() {
-    var that = this;
-    this.body = document.getElementById(this.data.body)
-    console.log("body", this.body);
+    var data = this.data;
+    var el = this.el;
+    this.body = document.getElementById(data.body)
+    console.log("body", data.body);
+    console.log("id", data.id);
+    el.setAttribute('constraint',
+                    'type: lock; maxForce: 0.9; target:' + data.id);
   },
-  tick: function () {
-
+  update: function () {
     
     }  
 }); 
