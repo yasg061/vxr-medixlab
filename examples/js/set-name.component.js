1f370cc287addfd2d7c2c2524be258767c5f6173
Orlando-Sanchez <or.sanchez100@gmail.com>
2021-05-04 15:24:37 -0700

    Create 'vxr-spectator-avatar-test.html' with spectator avatar name and models

--- examples/js/set-name.component.js
+++ examples/js/set-name.component.js
@@ -1,7 +1,15 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const userName = urlParams.get('name');
+const spawn = urlParams.get('spawn');
+
 document.addEventListener('DOMContentLoaded', () => {                           
   const scene = document.querySelector('a-scene');
   const sceneLoaded = () => {
-  document.getElementById("spectator").setAttribute("networked", "template:#spectator-avatar-template;attachTemplateToLocal:false;");
+    if ( userName && spawn ) {
+        document.getElementById("spectator").setAttribute("networked", "template:#spectator-avatar-template;attachTemplateToLocal:false;");
+        document.getElementById("constraint-spectator").setAttribute("networked", "template:#constraint-spectator-avatar-template;attachTemplateToLocal:false;");
+    }
   }
   if (scene.hasLoaded) {                                                         
     sceneLoaded();
@@ -10,17 +18,11 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const userName = urlParams.get('name')
-console.log('username', userName)
-
 AFRAME.registerComponent('name-up', {
     
     schema: {
         name: { default: userName }
     },
-
 
     update: function () {
         var el = this.el;
@@ -33,7 +35,7 @@ AFRAME.registerComponent('name-up', {
             '0 180 0',
         );
         el.setAttribute('position', {
-            x: 0.5, y: 2, z: 0,
+            x: 0.4, y: 0.25, z: 0,
         });
         el.setAttribute('scale',
             '0.32 0.32 0.32',
