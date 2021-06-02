b0ec6b6da832d22a373e320189dde75c4e397490
bryandrm <bryandrm97@gmail.com>
2021-06-02 09:31:57 -0600

    Modified Feature-Schemas.js to consume a json and spawn every naf schema.

--- examples/js/Feature-Schemas.js
+++ examples/js/Feature-Schemas.js
@@ -1,344 +1,40 @@
-//======================================================================
-//===============================maskn95=================================
-//======================================================================
+let result
+let obj
 
-let schema1={
-  template: '#vxr-obj-template',
-  components: [
-    'position',
-    'rotation',
-    {
-      selector: '.vxr-obj-maskn95',
-      component: 'rotation'
-    },
-    {
-      selector: '.vxr-obj-maskn95',
-      component: 'position'
+fetch("../../js/tst.json").then(
+  function(u){ return u.json();}
+).then(
+  function(json){
+    obj=json
+    console.log("json: ", obj)
+    result = JSON.parse(JSON.stringify(json));
+
+    SetSchemas(result)
+  }
+)
+
+function SetSchemas(structure) {
+  console.log("structure",structure)
+  structure.forEach(item => {
+
+    let schema1 = {
+      template: item.template,
+      components: [
+        'position',
+        'rotation',
+        {
+          selector: item.selector,
+          component: 'rotation'
+        },
+        {
+          selector: item.selector,
+          component: 'position'
+        }
+      ]
     }
-  ]
+    NAF.schemas.add(schema1);
+    console.log("added template: ", item.template)
+
+
+  });
 }
-
-
-NAF.schemas.add(schema1);
-
-//======================================================================
-//===============================alcoholbottle=================================
-//======================================================================
-
-let schema2={
-  template: '#vxr-obj-template-2',
-  components: [
-    'position',
-    'rotation',      
-    {
-      selector: '.vxr-obj-alcoholbottle',
-      component: 'rotation'
-    },
-    {
-      selector: '.vxr-obj-alcoholbottle',
-      component: 'position'
-    }
-  ]
-}
-
-NAF.schemas.add(schema2);
-
-
-
-//======================================================================
-//===============================tray=================================
-//======================================================================
-
-let shcema3={
-  template: '#vxr-obj-template-3',
-  components: [
-    'position',
-    'rotation',      
-    {
-      selector: '.vxr-obj-tray',
-      component: 'rotation'
-    },
-    {
-      selector: '.vxr-obj-tray',
-      component: 'position'
-    } 
-  ]
-}
-
-NAF.schemas.add(shcema3);
-
-
-//======================================================================
-//===============================rubber=================================
-//======================================================================
-
-let rubber={
-  template: '#vxr-obj-template-rubber',
-  components: [
-    'position',
-    'rotation',      
-    {
-      selector: '.vxr-obj-rubber',
-      component: 'rotation'
-    },
-    {
-      selector: '.vxr-obj-rubber',
-      component: 'position'
-    } 
-  ]
-}
-
-NAF.schemas.add(rubber);
-
-
-//======================================================================
-//===============================dripbag=================================
-//======================================================================
-
-let dripbag={
-  template: '#vxr-obj-template-dripbag',
-  components: [
-    'position',
-    'rotation',      
-    {
-      selector: '.vxr-obj-dripbag',
-      component: 'rotation'
-    },
-    {
-      selector: '.vxr-obj-dripbag',
-      component: 'position'
-    } 
-  ]
-}
-
-NAF.schemas.add(dripbag);
-
-
-//======================================================================
-//===============================medicaltape=================================
-//======================================================================
-
-let medicaltape={
-  template: '#vxr-obj-template-medicaltape',
-  components: [
-    'position',
-    'rotation',      
-    {
-      selector: '.vxr-obj-medicaltape',
-      component: 'rotation'
-    },
-    {
-      selector: '.vxr-obj-medicaltape',
-      component: 'position'
-    } 
-  ]
-}
-
-NAF.schemas.add(medicaltape);
-
-
-//======================================================================
-//===============================gauze=================================
-//======================================================================
-
-let gauze={
-  template: '#vxr-obj-template-gauze',
-  components: [
-    'position',
-    'rotation',      
-    {
-      selector: '.vxr-obj-gauze',
-      component: 'rotation'
-    },
-    {
-      selector: '.vxr-obj-gauze',
-      component: 'position'
-    } 
-  ]
-}
-
-NAF.schemas.add(gauze);
-
-
-//======================================================================
-//===============================allistweezers=================================
-//======================================================================
-
-let allistweezers={
-  template: '#vxr-obj-template-allistweezers',
-  components: [
-    'position',
-    'rotation',      
-    {
-      selector: '.vxr-obj-allistweezers',
-      component: 'rotation'
-    },
-    {
-      selector: '.vxr-obj-allistweezers',
-      component: 'position'
-    } 
-  ]
-}
-
-NAF.schemas.add(allistweezers);
-
-
-//======================================================================
-//===============================vxr-obj-template-cottonswab01=================================
-//======================================================================
-
-let cottonswab01={
-  template: '#vxr-obj-template-cottonswab01',
-  components: [
-    'position',
-    'rotation',      
-    {
-      selector: '.vxr-obj-cottonswab01',
-      component: 'rotation'
-    },
-    {
-      selector: '.vxr-obj-cottonswab01',
-      component: 'position'
-    } 
-  ]
-}
-
-NAF.schemas.add(cottonswab01);
-
-
-//======================================================================
-//===============================cottonswab02=================================
-//======================================================================
-
-let cottonswab02={
-  template: '#vxr-obj-template-cottonswab02',
-  components: [
-    'position',
-    'rotation',      
-    {
-      selector: '.vxr-obj-cottonswab02',
-      component: 'rotation'
-    },
-    {
-      selector: '.vxr-obj-cottonswab02',
-      component: 'position'
-    } 
-  ]
-}
-
-NAF.schemas.add(cottonswab02);
-
-
-//======================================================================
-//===============================cottonswab03=================================
-//======================================================================
-
-let cottonswab03={
-  template: '#vxr-obj-template-cottonswab03',
-  components: [
-    'position',
-    'rotation',      
-    {
-      selector: '.vxr-obj-cottonswab03',
-      component: 'rotation'
-    },
-    {
-      selector: '.vxr-obj-cottonswab03',
-      component: 'position'
-    } 
-  ]
-}
-
-NAF.schemas.add(cottonswab03);
-
-
-//======================================================================
-//===============================salinebottle=================================
-//======================================================================
-
-let salinebottle={
-  template: '#vxr-obj-template-salinebottle',
-  components: [
-    'position',
-    'rotation',      
-    {
-      selector: '.vxr-obj-salinebottle',
-      component: 'rotation'
-    },
-    {
-      selector: '.vxr-obj-salinebottle',
-      component: 'position'
-    } 
-  ]
-}
-
-NAF.schemas.add(salinebottle);
-
-
-//======================================================================
-//===============================alcoholgel=================================
-//======================================================================
-
-let alcoholgel={
-  template: '#vxr-obj-template-alcoholgel',
-  components: [
-    'position',
-    'rotation',      
-    {
-      selector: '.vxr-obj-alcoholgel',
-      component: 'rotation'
-    },
-    {
-      selector: '.vxr-obj-alcoholgel',
-      component: 'position'
-    } 
-  ]
-}
-
-NAF.schemas.add(alcoholgel);
-
-
-//======================================================================
-//===============================heparinseal=================================
-//======================================================================
-
-let heparinseal={
-  template: '#vxr-obj-template-heparinseal',
-  components: [
-    'position',
-    'rotation',      
-    {
-      selector: '.vxr-obj-heparinseal',
-      component: 'rotation'
-    },
-    {
-      selector: '.vxr-obj-heparinseal',
-      component: 'position'
-    } 
-  ]
-}
-
-NAF.schemas.add(heparinseal);
-
-
-//======================================================================
-//===============================intracath=================================
-//======================================================================
-
-let intracath={
-  template: '#vxr-obj-template-intracath',
-  components: [
-    'position',
-    'rotation',      
-    {
-      selector: '.vxr-obj-intracath',
-      component: 'rotation'
-    },
-    {
-      selector: '.vxr-obj-intracath',
-      component: 'position'
-    } 
-  ]
-}
-
-NAF.schemas.add(intracath);
