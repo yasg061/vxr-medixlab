df8020af3b1646a85c81ac197ee48edd30fe69d3
bryandrm <bryandrm97@gmail.com>
2021-06-02 14:17:51 -0600

    Cleaned spawnObjects script

--- examples/js/Feature-SpawnTemplate.js
+++ examples/js/Feature-SpawnTemplate.js
@@ -1,7 +1,7 @@
 
  
 
- AFRAME.registerComponent('setemplatecomponents', {
+ AFRAME.registerComponent('Setemplatecomponents', {
   schema: {
     speed: { default: 0.01 },
     direction: { default: 1 }
@@ -10,7 +10,7 @@
   init() {     
     var sceneEl = document.querySelector('a-scene');
     var entityEl = document.createElement('a-entity');
-    entityEl.setAttribute("id","mascarilla?")
+    entityEl.setAttribute("id","mask")
     entityEl.setAttribute("networked", "template:#vxr-obj-template;attachTemplateToLocal:true")
     entityEl.setAttribute("position","0 1.5 -0.2")
     sceneEl.appendChild(entityEl);
@@ -24,94 +24,42 @@ var a_assets = document.querySelector('a-assets');
 // create asset reference and set gltf atributes
 var maskn95= document.createElement('a-asset-item');
 maskn95.setAttribute("id","model-reference")
-maskn95.setAttribute("src","../../Models/gltf/Generic-model.gltf")
+maskn95.setAttribute("src","../../Models/gltf/maskn95.gltf")
 a_assets.appendChild(maskn95);
 
 // set template
 var mask_template = document.createElement('template');
-var entityEl = document.createElement('a-entity');
-var entityEl2 = document.createElement('a-entity');
+var rootEntity = document.createElement('a-entity');
+var bodyEntity = document.createElement('a-entity');
 
 
 mask_template.setAttribute("id","vxr-obj-template")
 a_assets.appendChild(mask_template);
 
 var templateAdded =document.getElementById("vxr-obj-template")
-entityEl.setAttribute("id","Root-mask")
-templateAdded.appendChild(entityEl)
+rootEntity.setAttribute("id","Root-mask")
+templateAdded.appendChild(rootEntity)
 
 
 var root_mask =document.getElementById("Root-mask")
-entityEl2.setAttribute("setemplatecomponents","")
-entityEl2.setAttribute('class', 'vxr-obj-maskn95 vxr-interact')
-entityEl2.setAttribute("gltf-model","#maskn95")
-entityEl2.setAttribute('draggable', '')
-entityEl2.setAttribute('graggable', '')
-entityEl2.setAttribute('toggle-physics', '')
-entityEl2.setAttribute('position', '-0.4 2 0')
-console.log("Elemento modificado?",entityEl2)
-root_mask.appendChild(entityEl2);
+bodyEntity.setAttribute("setemplatecomponents","")
+bodyEntity.setAttribute('class', 'vxr-obj-maskn95 vxr-interact')
+bodyEntity.setAttribute("gltf-model","#maskn95")
+bodyEntity.setAttribute('draggable', '')
+bodyEntity.setAttribute('graggable', '')
+bodyEntity.setAttribute('toggle-physics', '')
+bodyEntity.setAttribute('position', '-0.4 2 0')
+console.log("Element modified ",bodyEntity)
+root_mask.appendChild(bodyEntity);
 console.log("executed")
 
-
+// spawn entity
 var sceneEl = document.querySelector('a-scene');
-var entityEl = document.createElement('a-entity');
-entityEl.setAttribute("id","mascarilla?")
-entityEl.setAttribute("networked", "template:#vxr-obj-template;attachTemplateToLocal:true")
-entityEl.setAttribute("position","0 1.5 -0.2")
-sceneEl.appendChild(entityEl);
+var spawnNewEntity = document.createElement('a-entity');
+spawnNewEntity.setAttribute("id","mask-root")
+spawnNewEntity.setAttribute("networked", "template:#vxr-obj-template;attachTemplateToLocal:true")
+spawnNewEntity.setAttribute("position","0 1.5 -0.2")
+sceneEl.appendChild(spawnNewEntity);
 
 
 
-
-// var sceneEl = document.querySelector('a-scene');
-// var template = document.createElement('template');
-// var entityEl = document.createElement('a-entity');
-
-// template.setAttribute("id","vxr-obj-template")
-// entityEl.setAttribute('class', 'vxr-obj-maskn95 vxr-interact')
-// entityEl.setAttribute('gltf-model', '#maskn95')
-// entityEl.setAttribute('draggable', '')
-// entityEl.setAttribute('graggable', '')
-// entityEl.setAttribute('toggle-physics', '')
-// entityEl.setAttribute('position', '-0.6 2 -0.107')
-
-// template.appendChild(entityEl)
-// sceneEl.appendChild(template);
-
-// var template2 = document.createElement('template');
-// var entityEl2 = document.createElement('a-entity');
-
-// template2.setAttribute("id","vxr-obj-template-2")
-// entityEl2.setAttribute('class', 'vxr-obj-alcoholbottle vxr-interact')
-// entityEl2.setAttribute('gltf-model', '#alcoholbottle')
-// entityEl2.setAttribute('draggable', '')
-// entityEl2.setAttribute('graggable', '')
-// entityEl2.setAttribute('toggle-physics', '')
-// entityEl2.setAttribute('position', '-0.4 2 0')
-
-// template2.appendChild(entityEl2)
-// sceneEl.appendChild(template2);
-
-// var template3 = document.createElement('template');
-// var entityEl3 = document.createElement('a-entity');
-
-// template3.setAttribute("id","vxr-obj-template-3")
-// sceneEl3.setAttribute('class', 'vxr-obj-tray vxr-interact')
-// sceneEl3.setAttribute('gltf-model', '#tray')
-// sceneEl3.setAttribute('draggable', '')
-// sceneEl3.setAttribute('graggable', '')
-// sceneEl3.setAttribute('toggle-physics', '')
-// sceneEl3.setAttribute('position', '0.035 0.5 -0.2')
-
-// template3.appendChild(entityEl)
-// sceneEl3.appendChild(template);
-
-
-
-
-
-
-
-
-      
