1ecf30958b1461d563809beb1d84f8d0c0f4d8c9
bryandrm <bryandrm97@gmail.com>
2021-06-07 15:20:48 -0600

    cleaned student and teacher files

--- examples/js/Feature-SpawnTemplate.js
+++ examples/js/Feature-SpawnTemplate.js
@@ -1,6 +1,3 @@
-
- 
-
  AFRAME.registerComponent('Setemplatecomponents', {
   schema: {
     speed: { default: 0.01 },
@@ -8,40 +5,40 @@
   },
   
   init() {     
-    var sceneEl = document.querySelector('a-scene');
-    var entityEl = document.createElement('a-entity');
-    entityEl.setAttribute("id","mask")
-    entityEl.setAttribute("networked", "template:#vxr-obj-template;attachTemplateToLocal:true")
-    entityEl.setAttribute("position","0 1.5 -0.2")
-    sceneEl.appendChild(entityEl);
+    // var sceneEl = document.querySelector('a-scene');
+    // var entityEl = document.createElement('a-entity');
+    // entityEl.setAttribute("id","mask")
+    // entityEl.setAttribute("networked", "template:#vxr-obj-template;attachTemplateToLocal:true")
+    // entityEl.setAttribute("position","0 1.5 -0.2")
+    // sceneEl.appendChild(entityEl);
   },
 });
 
 
 // get a-assets section
-var a_assets = document.querySelector('a-assets');
+var aAssets = document.querySelector('a-assets');
 
 // create asset reference and set gltf atributes
 var maskn95= document.createElement('a-asset-item');
 maskn95.setAttribute("id","model-reference")
 maskn95.setAttribute("src","../../Models/gltf/maskn95.gltf")
-a_assets.appendChild(maskn95);
+aAssets.appendChild(maskn95);
 
 // set template
-var mask_template = document.createElement('template');
+var maskTemplate = document.createElement('template');
 var rootEntity = document.createElement('a-entity');
 var bodyEntity = document.createElement('a-entity');
 
 
-mask_template.setAttribute("id","vxr-obj-template")
-a_assets.appendChild(mask_template);
+maskTemplate.setAttribute("id","vxr-obj-template")
+aAssets.appendChild(maskTemplate);
 
 var templateAdded =document.getElementById("vxr-obj-template")
 rootEntity.setAttribute("id","Root-mask")
 templateAdded.appendChild(rootEntity)
 
 
-var root_mask =document.getElementById("Root-mask")
+var rootMask =document.getElementById("Root-mask")
 bodyEntity.setAttribute("setemplatecomponents","")
 bodyEntity.setAttribute('class', 'vxr-obj-maskn95 vxr-interact')
 bodyEntity.setAttribute("gltf-model","#maskn95")
@@ -50,16 +47,16 @@ bodyEntity.setAttribute('graggable', '')
 bodyEntity.setAttribute('toggle-physics', '')
 bodyEntity.setAttribute('position', '-0.4 2 0')
 console.log("Element modified ",bodyEntity)
-root_mask.appendChild(bodyEntity);
+rootMask.appendChild(bodyEntity);
 console.log("executed")
 
 // spawn entity
-var sceneEl = document.querySelector('a-scene');
+var rootEscene = document.querySelector('a-scene');
 var spawnNewEntity = document.createElement('a-entity');
 spawnNewEntity.setAttribute("id","mask-root")
 spawnNewEntity.setAttribute("networked", "template:#vxr-obj-template;attachTemplateToLocal:true")
 spawnNewEntity.setAttribute("position","0 1.5 -0.2")
-sceneEl.appendChild(spawnNewEntity);
+rootEscene.appendChild(spawnNewEntity);
 
 
 
