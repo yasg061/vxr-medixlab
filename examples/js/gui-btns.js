436cd0f999fb48af22669d1169a8c026d11f2230
S662 <or.sanchez100@gmail.com>
2021-06-23 16:46:00 -0400

    Add aframe-gui examples

---
+++ examples/js/gui-btns.js
@@ -0,0 +1,10 @@
+window.selectOption1 = function() {
+
+var option1 = document.getElementById("btn1");
+var option2 = document.getElementById("btn2");
+var option3 = document.getElementById("btn3");
+var option4 = document.getElementById("btn4");
+
+option2.components['gui-button'].setActiveState(false);
+option3.components['gui-button'].setActiveState(false);
+option4.components['gui-button'].setActiveState(false);}
