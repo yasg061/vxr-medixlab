bdf75701181f1d54290f7915d573e616bdddf89b
EnriqueVega1995 <vegacampos1995@gmail.com>
2021-07-01 15:03:57 -0600

    Added validation to catheter attach

--- examples/js/attach-point.js
+++ examples/js/attach-point.js
@@ -1,17 +1,22 @@
 AFRAME.registerComponent('attach-point', {
-    init: function() {
-      var el = this.el;
-      let cathter01 = document.getElementById('catheter001');
-      let cathter02 = document.getElementById('catheter002');
-      console.log('************************************************ ', cathter01);
-      this.el.addEventListener('collide', function(e) {
-        console.log('Syringe has collided with ', e.detail.body.el);
+  init: function() {
+    var el = this.el;
+    let cathter01 = document.getElementById('catheter001');
+    let cathter02 = document.getElementById('catheter002');
+    console.log('************************************************ ', cathter02);
+    this.el.addEventListener('collide', function(e) {
+      console.log('Syringe has collided with ', e.detail.body.el);
+      let attachCatheter = document.getElementById('vxr-intracath');
+      console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa ', attachCatheter);
+      console.log('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb ', e.detail.body.el);
+      if(e.detail.body.el==attachCatheter){
         e.detail.body.el.setAttribute('constraint','target: #sphere1');
         e.detail.body.el.setAttribute('visible','false');
         e.detail.body.el.remove();
         cathter02.setAttribute('dynamic-body', 'mass: 0');
         cathter01.setAttribute('visible', 'true');
         cathter02.setAttribute('visible', 'true');
-      });
-    }
-  })
+      }        
+    });
+  }
+})
