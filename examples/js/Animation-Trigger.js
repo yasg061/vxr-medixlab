5d86fb6b952c7b6bfaae5e3ae34833efa6540afa
bryandrm <bryandrm97@gmail.com>
2021-07-05 10:41:23 -0600

    Added script for component replication.

---
+++ examples/js/Animation-Trigger.js
@@ -0,0 +1,19 @@
+let DevilTrigger = false;
+AFRAME.registerComponent('animation-trigger', {
+  schema: {
+    triggerKeyCode: { default: 32 },
+    boolVar: { default: false } // spacebar
+  },
+  
+  init: function () {
+  },
+
+  update: function () {
+    var that = this;
+    console.log(this.data.boolVar)
+    if (this.data.boolVar) {
+      // let instruments = document.getElementById("instruments")
+      this.el.setAttribute("animation-mixer", "clampWhenFinished:true;")
+    }   
+  }
+})
