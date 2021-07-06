55ae3f22ecabf023aa8440bfc38a02bca299aa2f
EnriqueVega1995 <vegacampos1995@gmail.com>
2021-07-05 19:10:56 -0600

    Added new attach point with validations

--- examples/js/attach-point.js
+++ examples/js/attach-point.js
@@ -1,22 +1,51 @@
 AFRAME.registerComponent('attach-point', {
   init: function() {
     var el = this.el;
-    let cathter01 = document.getElementById('catheter001');
-    let cathter02 = document.getElementById('catheter002');
-    console.log('************************************************ ', cathter02);
+    
     this.el.addEventListener('collide', function(e) {
+      let cathter01 = document.getElementById('catheter001');
+      let cathter02 = document.getElementById('catheter002');
+      let medicalClote01 = document.getElementById('medicalclothe01');
+      let medicalClote02 = document.getElementById('medicalclothe02');
+      let dinamicTape01 = document.getElementById('tape01');
+      let dinamicTape02 = document.getElementById('tape02');
+      let dinamicTape03 = document.getElementById('tape03');
+      let staticTape01 = document.getElementById('tapestatic01');
+      let staticTape02 = document.getElementById('tapestatic02');
+      let staticTape03 = document.getElementById('tapestatic03');
+      let sphere01 = document.getElementById('sphere1');
+      let sphere02 = document.getElementById('sphere2');
+      console.log('************************************************ ', cathter02);
       console.log('Syringe has collided with ', e.detail.body.el);
       let attachCatheter = document.getElementById('vxr-intracath');
       console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa ', attachCatheter);
       console.log('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb ', e.detail.body.el);
       if(e.detail.body.el==attachCatheter){
-        e.detail.body.el.setAttribute('constraint','target: #sphere1');
-        e.detail.body.el.setAttribute('visible','false');
-        e.detail.body.el.remove();
         cathter02.setAttribute('dynamic-body', 'mass: 0');
         cathter01.setAttribute('visible', 'true');
         cathter02.setAttribute('visible', 'true');
-      }        
+        e.detail.body.el.setAttribute('constraint','target: #sphere1');
+        e.detail.body.el.setAttribute('visible','false');
+        e.detail.body.el.remove();
+        sphere01.setAttribute('position','0.89964 1.81662 -0.7305');
+      }if(e.detail.body.el==medicalclothe01){
+        medicalClote01.setAttribute('visible','false');
+        sphere02.setAttribute('position','0.89964 15 -0.7305');
+        medicalClote02.setAttribute('visible','true');
+        medicalClote02.setAttribute('dynamic-body', 'mass: 0');
+      }if(e.detail.body.el==dinamicTape01){
+        dinamicTape01.remove();
+        staticTape01.setAttribute('visible','true');
+      }if(e.detail.body.el==dinamicTape02){
+        dinamicTape02.remove();
+        staticTape02.setAttribute('visible','true');
+      }if(e.detail.body.el==dinamicTape03){
+        dinamicTape03.remove();
+        staticTape03.setAttribute('visible','true');
+      }     
     });
+  },
+  update: function(oldData) {
+    console.log("UPDATE");
   }
 })
