98c732ef74824c130cdb030a4d8978520d0db576
EnriqueVega1995 <vegacampos1995@gmail.com>
2021-07-27 17:03:11 -0600

    Added validation to attach points

--- examples/js/attach-intracath.js
+++ examples/js/attach-intracath.js
@@ -7,15 +7,18 @@ AFRAME.registerComponent('attach-intracath', {
     let classes= e.detail.body.el.className.split(' ')
     if(classes.includes("vxr-obj-intracath")){
     console.log('funca ', e.detail.body.el);
-
+    
+        e.detail.body.el.remove();
+        console.log('Syringe has collided with ', e.detail.body.el);
         let intracathInit =document.getElementById("firstPart")
         let intracathEnd=document.getElementById("vxr-intracath-second-part")
 
         intracathEnd.setAttribute('visible','true')
         intracathInit.setAttribute('visible','true')
         intracathEnd.setAttribute('dynamic-body','mass:0.0')
-        console.log('Syringe has collided with ', e.detail.body.el);
-        e.detail.body.el.remove();
+        e.detail.body.el.setAttribute("visible","false")
+        
+
       }else{
         console.log('no funca ', e.detail.body.el);
 
