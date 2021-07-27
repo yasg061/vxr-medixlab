5a9c5a6da72d6faea866e2c44ab2a59a8a8db769
bryandrm <bryandrm97@gmail.com>
2021-07-27 11:42:38 -0600

    added new assets on process8, teacher scene.

--- examples/js/Feature-SetAttributes.js
+++ examples/js/Feature-SetAttributes.js
@@ -1,8 +1,11 @@
-AFRAME.registerComponent('set-position', {
+AFRAME.registerComponent('Feature-SetAttributes', {
   schema: {
+    instance:{default:null}
   },
   
   init: function () {
+    let data = this.data
+    console.log("this is the data:", data)
     this.el.setAttribute('position','1 3 0')
     this.el.setAttribute('gltf-model','../../Models/gltf/maskn95.gltf')
   }, 
