f3303a527da97705dfeed9d44fbe580446116eb5
bryandrm <bryandrm97@gmail.com>
2021-08-20 20:11:04 -0600

    added components to mannage bones.

--- examples/js/Feature-Bones.component.js
+++ examples/js/Feature-Bones.component.js
@@ -3,10 +3,11 @@ AFRAME.registerComponent('bone-component', {
     triggerKeyCode: { default: 32 } // spacebar
   },
   init: function () {
-    let meshId=this.el.id
-    let fatherMesh = this.el.getAttribute('position');
-    console.log("father mesh position: ", fatherMesh)
+
     this.el.addEventListener('model-loaded', () => {
+      let meshId=this.el.id
+      let fatherMesh = this.el.getAttribute('position');
+      console.log("father mesh position: ", fatherMesh)
       let sceneEl = document.getElementById("boneToTest");
       // let sceneEl = document.querySelector("a-scene");
       let mesh = this.el.getObject3D("mesh")
@@ -39,24 +40,26 @@ AFRAME.registerComponent('bone-component', {
         objToIterate.forEach(bone => {
 
           let dir = new THREE.Vector3(); // create once an reuse it
-          
-          console.log("bone world position: ",bone.getWorldPosition())
-          console.log("mesh world position: ",fatherMesh)
-          console.log("mesh world position length: ",fatherMesh.length())
-
           // let pos =  bone.getWorldPosition() - fatherMesh
           dir.subVectors( bone.getWorldPosition(), fatherMesh );
-          
-          console.log("result position: ",dir)
 
-          console.log("bone: ", bone)
+
           let entityEl = document.createElement('a-box')
+          let rot = { x: THREE.Math.radToDeg(bone.rotation.x), y:THREE.Math.radToDeg(bone.rotation.y), z:THREE.Math.radToDeg(bone.rotation.z) }
+
+          // console.log("bone world position: ",bone.getWorldPosition())
+          // console.log("mesh world position: ",fatherMesh)
+          // console.log("mesh world position length: ",fatherMesh.length())
+          // console.log("result position: ",dir)
+          // console.log("bone: ", bone)
+          // console.log("rotation: ", rot)
 
           entityEl.setAttribute("id", bone.name)
           entityEl.setAttribute("dynamic-body", "mass:0.0")
-          entityEl.setAttribute("geometry", "width:0.05; height:0.05;depth:0.05")
+          entityEl.setAttribute("geometry", "width:0.1; height:0.1;depth:0.1")
           //entityEl.setAttribute("position", bone.position)
           entityEl.setAttribute("position", dir)
+          entityEl.setAttribute("rotation", rot)
            entityEl.setAttribute("opacity", "1")
           entityEl.setAttribute("grabbable", "")
           entityEl.setAttribute("draggable", "")
--- examples/js/Feature-BonesPosition.component.js
+++ examples/js/Feature-BonesPosition.component.js
@@ -20,11 +20,11 @@ AFRAME.registerComponent('boneposition', {
     this.data.bone=boneReference;       
     // boneReference.position.set(x)//=   new THREE.Vector3( 0, 5, 0 );
     let pos = this.el.getAttribute('position')
-    console.log("bone reference: ",boneReference.position)
-    console.log("entity position",this.el.getAttribute('position'))
-    console.log("entity rotation",this.el.getAttribute('rotation'))
-    console.log("mesh: ",boneReference)
-    console.log("entity: ", this.el.getObject3D("mesh"))
+    // console.log("bone reference: ",boneReference.position)
+    // console.log("entity position",this.el.getAttribute('position'))
+    // console.log("entity rotation",this.el.getAttribute('rotation'))
+    // console.log("mesh: ",boneReference)
+    // console.log("entity: ", this.el.getObject3D("mesh"))
     let dir = new THREE.Vector3(); // create once an reuse it
     this.data.vectorPosition=dir
 
@@ -42,16 +42,24 @@ var dir = new THREE.Vector3(); // create once an reuse it
        let worldQuaternion = new THREE.Quaternion();
     let entityQuat = this.el.object3D.getWorldQuaternion(worldQuaternion);
     let pos = this.el.getObject3D("mesh").getWorldPosition()
-    dir.subVectors( pos , this.data.bone.getWorldPosition()).normalize();
+    // dir.subVectors(  pos, this.data.bone.getWorldPosition()  ).normalize() ;
+    dir= this.data.bone.worldToLocal(pos).normalize() ;
+    
 
-    if(dir.length()> 1 ){
-     
-      this.data.bone.position.copy(dir);
-      console.log(dir.length())
-    }else{
-      this.data.varBool=true   
+    
+    // if(dir.length()>1){
+    // if(this.data.varBool){
 
-     }
+    //   this.data.bone.position.copy(dir);
+    //   console.log("bone: ", this.data.bone.position)
+    //   console.log("bone world position: ", this.data.bone.getWorldPosition())
+    //   console.log("entity position: ", pos)
+    // }
+    //   console.log(dir.length())
+    // else{
+    //   this.data.varBool=true   
+
+    //  }
     
     this.data.bone.rotation.setFromQuaternion(entityQuat)
     
