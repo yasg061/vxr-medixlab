70c349dd6249d516dca84242f296fa3e54a07846
EnriqueVega1995 <vegacampos1995@gmail.com>
2021-10-15 09:04:52 -0600

    Added characters

--- examples/js/raycast.component.js
+++ examples/js/raycast.component.js
@@ -50,9 +50,27 @@ AFRAME.registerComponent('gltf-hover', {
         // intersection.object.material.emissive = new THREE.Color(0xFF0000);
         // intersection.object.material.emissiveIntensity = 0.5;
         if (intersection.object.name == 'Box000') {
-          document.getElementById("e-Egg").setAttribute('animation-mixer', 'clip:Idle')
+          document.getElementById('e-Egg').setAttribute('animation-mixer', 'clip:Idle')
         } else if (intersection.object.name == 'Box001') {
-          document.getElementById("e-BadChicken").setAttribute('animation-mixer', 'clip:Idle')
+          document.getElementById('e-BadChicken').setAttribute('animation-mixer', 'clip:Idle')
+        } else if (intersection.object.name == 'Box002') {
+          document.getElementById('e-Bat').setAttribute('animation-mixer', 'clip:Idle')
+        } else if (intersection.object.name == 'Box003') {
+          document.getElementById('e-Butterfly').setAttribute('animation-mixer', 'clip:Idle')
+        } else if (intersection.object.name == 'Box004') {
+          document.getElementById('e-Cactus').setAttribute('animation-mixer', 'clip:Idle')
+        } else if (intersection.object.name == 'Box005') {
+          document.getElementById('e-Ghost').setAttribute('animation-mixer', 'clip:Idle')
+        } else if (intersection.object.name == 'Box006') {
+          document.getElementById('e-Gremlin').setAttribute('animation-mixer', 'clip:Idle')
+        } else if (intersection.object.name == 'Box007') {
+          document.getElementById('e-Ogre').setAttribute('animation-mixer', 'clip:Idle')
+        } else if (intersection.object.name == 'Box008') {
+          document.getElementById('e-Penguin').setAttribute('animation-mixer', 'clip:Idle')
+        } else if (intersection.object.name == 'Box009') {
+          document.getElementById('e-RockGuitar').setAttribute('animation-mixer', 'clip:Idle')
+        } else if (intersection.object.name == 'Box010') {
+          document.getElementById('e-Vampire').setAttribute('animation-mixer', 'clip:Idle')
         }
 
         if (self.mouseOverObject) {
@@ -62,6 +80,24 @@ AFRAME.registerComponent('gltf-hover', {
             document.getElementById('e-Egg').removeAttribute('animation-mixer')
           } else if (self.mouseOverObject.name == 'Box001') {
             document.getElementById('e-BadChicken').removeAttribute('animation-mixer')
+          } else if (self.mouseOverObject.name == 'Box002') {
+            document.getElementById('e-Bat').removeAttribute('animation-mixer')
+          } else if (self.mouseOverObject.name == 'Box003') {
+            document.getElementById('e-Butterfly').removeAttribute('animation-mixer')
+          } else if (self.mouseOverObject.name == 'Box004') {
+            document.getElementById('e-Cactus').removeAttribute('animation-mixer')
+          } else if (self.mouseOverObject.name == 'Box005') {
+            document.getElementById('e-Ghost').removeAttribute('animation-mixer')
+          } else if (self.mouseOverObject.name == 'Box006') {
+            document.getElementById('e-Gremlin').removeAttribute('animation-mixer')
+          } else if (self.mouseOverObject.name == 'Box007') {
+            document.getElementById('e-Ogre').removeAttribute('animation-mixer')
+          } else if (self.mouseOverObject.name == 'Box008') {
+            document.getElementById('e-Penguin').removeAttribute('animation-mixer')
+          } else if (self.mouseOverObject.name == 'Box009') {
+            document.getElementById('e-RockGuitar').removeAttribute('animation-mixer')
+          } else if (self.mouseOverObject.name == 'Box010') {
+            document.getElementById('e-Vampire').removeAttribute('animation-mixer')
           }
         }
         self.mouseOverObject = intersection.object;
