309fb7e269f4d545466b4e787653b533924e661b
bryandrm <bryandrm97@gmail.com>
2022-01-05 00:03:03 -0600

    Modified process6, incorporate a new medical kit.

--- examples/js/Feature-animation.component.js
+++ examples/js/Feature-animation.component.js
@@ -1,24 +1,22 @@
 
-let scene = document.querySelector("a-scene")
 
 
 AFRAME.registerComponent('feature-animation', {
   schema: {
-    obj_id: { default: '' }
+    obj_id: { default: '' },
     clip: { default: '' },
     loop: { default: '' },
     repetitions: { default: '' },
     clamp: { default: '' },
-    obj_id: { default: '' }
+    obj_id: { default: '' },
+    start: { default: false }
   },
   init: function () {
-
-
     console.log("init");
   },
 
   update: function () {
-
+    let scene = document.querySelector("a-scene")
     let data = this.data
     let obj = document.getElementById(data.obj_id)
     console.log("obj: ", obj)
@@ -26,16 +24,14 @@ AFRAME.registerComponent('feature-animation', {
     var jumpTrans = function () {
       scene.removeEventListener('animation-loop', jumpTrans);
       obj.setAttribute("animation-mixer",
-      `clip:${data.clip};
+        `clip:${data.clip};
       loop:${data.loop};
       repetitions:${data.repetitions};
       clampWhenFinished:${data.clamp}; `);
       console.log("start")
     };
-    setTimeout(function () { scene.addEventListener('animation-loop', jumpTrans); }, 5000);
-
+    if (data.start) {
+      setTimeout(function () { scene.addEventListener('animation-loop', jumpTrans); }, 5000);
+    }
   }
-
-
-
 });
